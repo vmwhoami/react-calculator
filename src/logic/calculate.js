4276b2b3b858bbdaa685fa3299f6d05a972f642e
vmwhoami <vmwhoami@gmail.com>
2021-02-14 15:59:10 +0200

    add switch statement to calculate fucnction

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,6 +1,22 @@
-import Big from 'big.js';
+
 const Calculate = (dataObj, btnName) => {
   const { total, next, operation } = dataObj
+  // 'AC', '+/-', '%', '÷'
+  switch (btnName) {
+    case 'AC': {
+      total = null;
+      next = null;
+      operation = null;
+    }
+    case '+/-': {
+      (total *= -1).toString();
+      (next *= -1).toString();
+    }
+    case '%': {
+      total *= 0.1
+    }
+  }
+  return { total, next, operation }
 }
 
 export default Calculate;
