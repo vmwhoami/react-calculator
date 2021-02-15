e69c13d504d183b2d7959c4262ef1760528effb5
vmwhoami <vmwhoami@gmail.com>
2021-02-15 21:16:58 +0200

    build the App class bind handleClick event

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display />
-    {Calculate}
-    <ButtonPanel />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    this.setState(prevState => {
+      const obj = { ...prevState };
+      return Calculate(obj, buttonName);
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Display />
+        <ButtonPanel />
+      </>
+    );
+  }
+}
 
 export default App;
