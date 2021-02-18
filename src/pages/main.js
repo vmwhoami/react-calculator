1ddd1c75be323ffaa284e4dabcda3d3de6be697d
vmwhoami <vmwhoami@gmail.com>
2021-02-18 12:13:00 +0200

    add quote page and liked it in the navbar

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Nav from '../components/Nav';
 import Home from './home';
 import Calculator from './calculator';
+import Quote from './quote';
 
 const Main = () => (
   <div className="container">
@@ -12,6 +13,7 @@ const Main = () => (
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/calculator" component={Calculator} />
+        <Route path="/quote" component={Quote} />
       </Switch>
     </BrowserRouter>
   </div>
---
+++ src/pages/quote.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Quote = () => (
+  <>
+    <h1>Quote of the day</h1>
+  </>
+);
+
+export default Quote;
