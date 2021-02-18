03141ce0dfe4aa745568cabadff79f06c1e629e7
vmwhoami <vmwhoami@gmail.com>
2021-02-18 11:42:26 +0200

    add the react router dom create pages folder

---
+++ src/components/Nav.js
@@ -0,0 +1,6 @@
+
+const Nav = () => {
+  return (
+  
+)
+}
---
+++ src/pages/calculator.js
@@ -0,0 +1,11 @@
+
+
+const Calculator = () => {
+  return (
+    <>
+      <h1>Let's do some math</h1>
+    </>
+  )
+}
+
+export default Calculator;
