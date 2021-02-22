84662edaa7113a080e7cd4c3c00bd53ec6a10a3c
vmwhoami <vmwhoami@gmail.com>
2021-02-22 17:25:14 +0200

    add main page testing

--- src/__test__/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import App from '../components/App';
-
-test('renders learn react link', () => {
-  render(<App />);
-
-  // expect(linkElement).toBeInTheDocument();
-});
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,6 @@ import { NavLink } from 'react-router-dom';
 const Nav = () => (
 
   <div className="nav-container">
-
     <NavLink to="/">
       <h1>Math magicians</h1>
     </NavLink>
